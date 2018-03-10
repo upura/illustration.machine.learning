26610549f111b753063af61f7f9349302aae8ae3
upura <upura0@gmail.com>
2018-03-10 23:51:37 +0900

    def MisClassificationError()

--- src/decision_stock.py
+++ src/decision_stock.py
@@ -9,6 +9,15 @@ np.random.seed(seed = 32)
 x = np.random.randn(N, CLASS_NUM)
 y = 2 * (x[:,0] > x[:,1]) - 1
 
+def calcMisclassificationError(d):
+    xs = np.sort(x[:,d])
+    xi = np.argsort(x[:,d])
+    el = np.cumsum(y[xi])
+    eu = np.cumsum(y[xi[::-1]])
+    e = eu[(N-1):0:-1] - el[0:(N-1)]
+    ei = int(np.max(np.abs(e)))
+    return xs[ei]
+
 if __name__ == "__main__":
     d = int(np.ceil(np.random.rand() * CLASS_NUM)) - 1
     xs = np.sort(x[:,d])
@@ -17,7 +26,7 @@ if __name__ == "__main__":
     eu = np.cumsum(y[xi[::-1]])
     e = eu[(N-1):0:-1] - el[0:(N-1)]
     ei = int(np.max(np.abs(e)))
-    c = xs[ei]
+    c = calcMisclassificationError(d)
     
     if d == 0:
         X0 = np.linspace(-3, 3, N)
